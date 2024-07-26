b8c547db69983a2a6bb95c5efabf7ef079e92d99
TaiseiHamaya <146906391+TaiseiHamaya@users.noreply.github.com>
2024-07-26 11:27:30 +0900

    Grayscale.PSの微修正

--- Engine/Render/RenderNode/Grayscale/Grayscale.PS.hlsl
+++ Engine/Render/RenderNode/Grayscale/Grayscale.PS.hlsl
@@ -8,7 +8,7 @@ float4 main(VertexShaderOutput input) : SV_TARGET0 {
 	float4 outputColor;
 	float4 sampleColor = gTexture.Sample(gSampler, input.texcoord);
 	if (gIsGray) {
-		float y = 0.33f * sampleColor.r + 0.33f * sampleColor.g + 0.34f * sampleColor.b;
+		float y = 0.3f * sampleColor.r + 0.58f* sampleColor.g + 0.12f * sampleColor.b;
 		outputColor = float4(y, y, y, 1.0f);
 	}
 	else {
