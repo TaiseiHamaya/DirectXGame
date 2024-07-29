a1af10ce1eb923b247b5d1e8b06dda94a7420f4e
TaiseiHamaya <146906391+TaiseiHamaya@users.noreply.github.com>
2024-07-29 10:14:17 +0900

    定数の追加を行いました。

--- Engine/Math/Definition.h
+++ Engine/Math/Definition.h
@@ -7,13 +7,15 @@ constexpr float PI = float(std::numbers::pi);
 constexpr float PI_H = float(std::numbers::pi * 0.5);
 constexpr float PI2 = float(std::numbers::pi * 2.0);
 
+constexpr float ToRadian = PI / 180.0f;
+constexpr float ToDegree = 180.0f / PI;
+
 /// <summary>
 /// メモリのdeleteとnullptr化
 /// <param name="mem">開放するメモリの変数</param>
 /// </summary>
 template<class type>
 constexpr void MemClear(type*& mem);
-
 
 ///-------------------///
 ///   ここから定義     ///
