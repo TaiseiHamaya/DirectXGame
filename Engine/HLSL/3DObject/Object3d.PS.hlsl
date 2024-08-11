5a53eb3b634cc49f1ec44729835f5223ff04e950
TaiseiHamaya <146906391+TaiseiHamaya@users.noreply.github.com>
2024-08-11 11:42:37 +0900

    Object3DのPSに、完全透明対応を行いました。

--- Engine/HLSL/3DObject/Object3d.PS.hlsl
+++ Engine/HLSL/3DObject/Object3d.PS.hlsl
@@ -29,15 +29,20 @@ PixelShaderOutput main(VertexShaderOutput input) {
 	else if (gMaterial.lightingType == 1) {
 		float NdotL = dot(normalize(input.normal), -gDirectionalLight.directon);
 		float cos = saturate(NdotL);
-		output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
+		output.color.rgb = gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
+		output.color.a = gMaterial.color.a * textureColor.a;
 	}
 	else if (gMaterial.lightingType == 2) {
 		float NdotL = dot(normalize(input.normal), -gDirectionalLight.directon);
 		float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
-		output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
+		output.color.rgb = gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
+		output.color.a = gMaterial.color.a * textureColor.a;
 	}
 	else {
 		output.color = float4(1.0f, 1.0f, 1.0f, 1.0f);
+	}
+	if (output.color.a == 0.0f) {
+		discard;
 	}
 	return output;
 }
