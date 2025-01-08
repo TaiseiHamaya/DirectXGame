8108ce54fad4fd685af4858d0280d7a1b5117694
TaiseiHamaya <146906391+TaiseiHamaya@users.noreply.github.com>
2025-01-08 14:04:08 +0900

    NodeAnimationの機能追加を行いました。

--- project/Engine/Resources/Animation/NodeAnimation/NodeAnimationResource.h
+++ project/Engine/Resources/Animation/NodeAnimation/NodeAnimationResource.h
@@ -45,6 +45,15 @@ public:
 
 	const Animation* animation(const std::string& animationName) const;
 
+#ifdef _DEBUG
+public:
+	bool animation_list_gui(std::string& current) const;
+#endif // _DEBUG
+
 private:
+#ifdef _DEBUG
+	std::map<std::string, Animation> animations;
+#else
 	std::unordered_map<std::string, Animation> animations;
+#endif // _DEBUG
 };
