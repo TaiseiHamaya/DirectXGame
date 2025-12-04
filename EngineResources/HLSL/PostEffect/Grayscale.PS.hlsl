6e01c24c0b93eb21929b9278f3e47ca6021e68aa
TaiseiHamaya <146906391+TaiseiHamaya@users.noreply.github.com>
2025-12-05 00:34:34 +0900

    デバッグ用に設定したパラメータの修正を行いました。

--- EngineResources/HLSL/PostEffect/Grayscale.PS.hlsl
+++ EngineResources/HLSL/PostEffect/Grayscale.PS.hlsl
@@ -7,7 +7,7 @@ uint gIsGray : register(b0);
 float4 main(VertexShaderOutput input) : SV_TARGET0 {
 	float4 outputColor;
 	float4 sampleColor = gTexture.Sample(gSampler, input.texcoord);
-	if (true) {
+	if (gIsGray) {
 		float y = 0.3f * sampleColor.r + 0.58f* sampleColor.g + 0.12f * sampleColor.b;
 		outputColor = float4(y, y, y, 1.0f);
 	}
